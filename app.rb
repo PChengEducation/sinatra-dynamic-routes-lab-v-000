4ffd8aefa50aeb86edf5670c17addfa9eb8958ab
Pan Cheng <pchengeducation@hotmail.com>
2018-08-22 01:39:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     "#{@phrase}\n" * @number
   end
- 
+
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
